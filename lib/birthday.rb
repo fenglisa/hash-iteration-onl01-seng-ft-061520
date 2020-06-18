54b9220d59a2ef5456ffbbabb99ad2f4c2793d7e
Lisa Feng <lisafeng10@gmail.com>
2020-06-18 21:34:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each {|name,age| puts "Happy Birthday #{name}! You are now #{age} years old!"}
+  birthday_kids.each do |name,age| 
+    if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+  end
 end
 
 
