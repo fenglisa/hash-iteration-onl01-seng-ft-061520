e8397fbcec6650d0869dfe824422048e3403f9bd
Lisa Feng <lisafeng10@gmail.com>
2020-06-18 21:37:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name,age| 
-    puts "Happy Birthday #{name}! You are now #{age} years old!".if age <12
+    if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
   end
 end
